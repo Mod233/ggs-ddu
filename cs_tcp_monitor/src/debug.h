9d83ac562f218c5faa358ac24113327bf3185a70
cs_ubuntu <ccccsober@gmail.com>
2018-06-26 21:22:19 +0800

    add_up_down

--- cs_tcp_monitor/src/debug.h
+++ cs_tcp_monitor/src/debug.h
@@ -4,15 +4,16 @@
 #define SHOW_SLICE_RESULT 0
 #define SHOW_SLICE_RESULT_AFTER_CUT 0
 #define SHOW_OUTCTL 0
-#define SHOW_HEARTPKT 1
+#define SHOW_HEARTPKT 0
 #define CUT_HEARTBEAT 1
-#define JUDGE_FILE 1
+#define JUDGE_FILE 0
 #define SHOW_CHILD_PATH 1
 #define SHOW_CK  0
 #define SHOW_CK_FINAL 0
 #define CONFIGDENT 0.6
 #define SHOW_DNS_VECTOR 1
 #define IP_SIZE 300000
+#define UP_DOWN 1
 
 extern unsigned int subnet_intranet; //存储子网ip，用于区分内部IP地址和外部IP地址
 extern unsigned int subnet_extranet; //存储子网ip，用于区分内部IP地址和外部IP地址
